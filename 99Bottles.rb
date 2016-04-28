8ff56c9457e304c13659c54ee57f759a301f51d3
kscotteng <kscotteng@gmail.com>
2016-04-27 22:56:05 -0400

    add 99bottles.rb

---
+++ 99Bottles.rb
@@ -0,0 +1,22 @@
+bottles = 99
+
+while bottles > 1
+  puts ''
+  puts bottles.to_s + ' bottles of beer on the wall,'
+  puts bottles.to_s + ' bottles of beer...'
+  puts ''
+  puts 'Take 1 down, pass it around,'
+  bottles = bottles - 1
+  if bottles > 1
+    puts bottles.to_s + ' bottles of beer on the wall.'
+  else
+    puts bottles.to_s + ' bottle of beer on the wall.'
+    puts ''
+    puts bottles.to_s + ' bottle of beer...'
+    puts bottles.to_s + ' Take it down...  Pass it around...'
+    puts bottles.to_s + ' No more beer you bums!'
+    puts ''
+  end
+end
+
+  
