f9d372acaae12c246ea05daad01147d3bbd8700c
kscotteng <kscotteng@gmail.com>
2016-05-02 21:52:38 -0400

    create range of years from start/end inputs

--- LeapYears.rb
+++ LeapYears.rb
@@ -5,3 +5,12 @@ puts ''
 puts 'Please enter an ending year:'
 ending_year = gets.chomp
 puts ''
+
+year_range = ending_year.downto(starting_year).to_a
+
+
+
+# Defininig leap years:
+# any year that is evenly divisible by 4 is a leap year
+# a year that is evenly divisble by both 100 & 400
+# END
