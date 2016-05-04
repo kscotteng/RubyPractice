74d3dc5fb9e7f90a4f251873c1c3cedb3fc82799
kscotteng <kscotteng@gmail.com>
2016-05-03 21:42:30 -0400

    works in irb, not as .rb... troubleshooting

--- LeapYears.rb
+++ LeapYears.rb
@@ -6,12 +6,15 @@ puts 'Please enter an ending year:'
 ending_year = gets.chomp
 puts ''
 
-year_range = [*starting_year..ending_year]
-puts year_range
+leap_year1 = [*starting_year..ending_year].delete_if { |y| y % 4 != 0 }
+puts leap_year1
 
 
+
+# .delete_if { |y| y % 4 != 0 }
 
 # Defininig leap years:
 # any year that is evenly divisible by 4 is a leap year
 # a year that is evenly divisble by both 100 & 400
+# a year that is evenly divisible only by 100 is NOT a leap year
 # END
