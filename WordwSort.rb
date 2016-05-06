31f73639203251f7d55002c8c38bcf869f9b51c2
kscotteng <kscotteng@gmail.com>
2016-05-05 21:30:29 -0400

    WordwSort - initial commit

---
+++ WordwSort.rb
@@ -0,0 +1,6 @@
+# Write a program which asks the user to enter random words.
+# One word per line. Continuing until the Enter key is pressed on an empty line.
+# The code will return all words entered in alphabetical order.
+# Hint: use the sort method.
+
+random_words = []
