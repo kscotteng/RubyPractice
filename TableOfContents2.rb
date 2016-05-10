51f23f7b5b9c67057d0df3a5d7146e1cbe91e270
kscotteng <kscotteng@gmail.com>
2016-05-09 21:19:28 -0400

    create 'info' array that contains all table of contents information

--- TableOfContents2.rb
+++ TableOfContents2.rb
@@ -1,3 +1,5 @@
 # Rewrite your Table of Contents program.
 # Start the program with an array holding all of the information for your Table of Contents (chapter names, page numbers, etc.).
 # Then print out the information from the array in a formatted Table of Contents.
+
+info = ['Chapter 1:  Numbers', 'Chapter 2:  Letters','Chapter 3:  Variables', 'page 1', 'page 72', 'page 118']
