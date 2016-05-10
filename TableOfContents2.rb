bb527ed4bb1941db95c2f389c6044521e753159a
kscotteng <kscotteng@gmail.com>
2016-05-09 21:11:11 -0400

    initial commit

---
+++ TableOfContents2.rb
@@ -0,0 +1,3 @@
+# Rewrite your Table of Contents program.
+# Start the program with an array holding all of the information for your Table of Contents (chapter names, page numbers, etc.).
+# Then print out the information from the array in a formatted Table of Contents.
