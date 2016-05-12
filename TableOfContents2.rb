fe0bc0e0076ecbef3968425083771fa3f1a8d616
kscotteng <kscotteng@gmail.com>
2016-05-12 11:32:16 -0400

    create output template

--- TableOfContents2.rb
+++ TableOfContents2.rb
@@ -8,3 +8,19 @@ info = ['Chapter 3:  Variables', 'page 1', 'Chapter 1:  Numbers','page 118', 'Ch
 # create sorted_info array and sort
 info.sort! {|c,v| c <=> v }
 puts info
+
+puts
+lineWidth = 50
+ch1 = '1: Numbers'
+ch2 = '2: Letters'
+ch3 = '3: Variables'
+ch1_pgs = '1'
+ch2_pgs = '72'
+ch3_pgs = '118'
+puts 'Table of Contents'.center(lineWidth)
+puts
+puts 'Chapter'.ljust(lineWidth/2) + 'Page'.rjust(lineWidth/2)
+puts
+puts ch1.ljust(lineWidth/2) + ch1_pgs.rjust(lineWidth/2)
+puts ch2.ljust(lineWidth/2) + ch2_pgs.rjust(lineWidth/2)
+puts ch3.ljust(lineWidth/2) + ch3_pgs.rjust(lineWidth/2)
