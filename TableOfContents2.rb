aed3df7552e19f5690e416d1a502b1593d58cd01
kscotteng <kscotteng@gmail.com>
2016-05-12 10:32:49 -0400

    randomize info array, begin sorting process

--- TableOfContents2.rb
+++ TableOfContents2.rb
@@ -2,4 +2,12 @@
 # Start the program with an array holding all of the information for your Table of Contents (chapter names, page numbers, etc.).
 # Then print out the information from the array in a formatted Table of Contents.
 
-info = ['Chapter 1:  Numbers', 'Chapter 2:  Letters','Chapter 3:  Variables', 'page 1', 'page 72', 'page 118']
+# create info array with all information out of order
+info = ['Chapter 3:  Variables', 'page 1', 'Chapter 1:  Numbers','page 118', 'Chapter 2:  Letters', 'page 72']
+
+# create sorted_info array and sort
+sorted_info = []
+  while info.length > 0
+  sorted_info << info.delete(info.min)
+  end
+puts sorted_info
