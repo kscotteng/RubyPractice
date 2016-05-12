bb2e7c88d29683e77c94354bcaeeb55017b652f4
kscotteng <kscotteng@gmail.com>
2016-05-12 13:10:59 -0400

    remove sorted info array, create chapters array from info array, sort chapters, comment out output formatting for now

--- TableOfContents2.rb
+++ TableOfContents2.rb
@@ -5,22 +5,24 @@
 # create info array with all information out of order
 info = ['3: Variables', '1', '1: Numbers','118', '2: Letters', '72']
 
-# create sorted_info array and sort
-info.sort! {|c,v| c <=> v }
-puts info
+chapters = []
+chapters << info.find_all {|a| a.length > 3}
+chapters.flatten!
+chapters.sort! {|c1,c2| c1 <=> c2}
+puts chapters
 
-puts
-lineWidth = 50
-ch1 = '1: Numbers'
-ch2 = '2: Letters'
-ch3 = '3: Variables'
-ch1_pgs = '1'
-ch2_pgs = '72'
-ch3_pgs = '118'
-puts 'Table of Contents'.center(lineWidth)
-puts
-puts 'Chapter'.ljust(lineWidth/2) + 'Page'.rjust(lineWidth/2)
-puts
-puts ch1.ljust(lineWidth/2) + ch1_pgs.rjust(lineWidth/2)
-puts ch2.ljust(lineWidth/2) + ch2_pgs.rjust(lineWidth/2)
-puts ch3.ljust(lineWidth/2) + ch3_pgs.rjust(lineWidth/2)
+# puts
+# lineWidth = 50
+# ch1 = '1: Numbers'
+# ch2 = '2: Letters'
+# ch3 = '3: Variables'
+# ch1_pgs = '1'
+# ch2_pgs = '72'
+# ch3_pgs = '118'
+# puts 'Table of Contents'.center(lineWidth)
+# puts
+# puts 'Chapter'.ljust(lineWidth/2) + 'Page'.rjust(lineWidth/2)
+# puts
+# puts ch1.ljust(lineWidth/2) + ch1_pgs.rjust(lineWidth/2)
+# puts ch2.ljust(lineWidth/2) + ch2_pgs.rjust(lineWidth/2)
+# puts ch3.ljust(lineWidth/2) + ch3_pgs.rjust(lineWidth/2)
