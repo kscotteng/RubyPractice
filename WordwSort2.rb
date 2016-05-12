d841d89ffe51385deb65c0bf72903dbd37ed2f25
kscotteng <kscotteng@gmail.com>
2016-05-11 21:59:06 -0400

    initial commit

---
+++ WordwSort2.rb
@@ -0,0 +1,36 @@
+# Re-write original Word Sort program to sort words without using the sort method.
+# Program asks the user to enter random words.
+# One word per line. Continuing until the Enter key is pressed on an empty line.
+# The code will return all words entered in alphabetical order.
+# Do NOT use the sort method this time.
+
+random_words = []
+puts ''
+puts 'Welcome to the Random Word Sorter...'
+puts ''
+puts 'Instructions:  Enter one (and only one) random word per line.'
+puts '               Once you have exhausted your vocabulary,'
+puts '               Simply hit the Enter key.'
+puts ''
+puts 'Your extensive vocabulary will then be listed in alphabetical order.'
+
+puts ''
+puts 'Enter one random word:'
+puts ''
+
+input = gets.chomp
+
+while input != "" do
+  random_words << input
+  puts ''
+  puts 'Enter another random word, or,'
+  puts 'simply press the Enter key to quit:'
+  puts ''
+  input = gets.chomp
+end
+  puts ''
+  puts 'Your words,'
+  puts 'sorted alphabetically:'
+  puts ''
+  puts random_words.sort { |a, b| a.upcase <=> b.upcase }
+  puts ''
