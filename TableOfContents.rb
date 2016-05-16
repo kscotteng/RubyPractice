e8475569269dffdd168ab3e198b17e284dbe2ff7
kscotteng <kscotteng@gmail.com>
2016-05-15 21:18:25 -0400

    update format

--- TableOfContents.rb
+++ TableOfContents.rb
@@ -11,8 +11,11 @@ ch3 = 'Chapter 3:  Variables'
 ch1_pgs = 'page 1'
 ch2_pgs = 'page 72'
 ch3_pgs = 'page 118'
-puts('Table of Contents'.center(lineWidth))
+puts 'Table of Contents'.center(lineWidth)
+puts
+puts 'Chapter'.ljust(lineWidth/2) + 'Page'.rjust(lineWidth/2)
 puts
 puts ch1.ljust(lineWidth/2) + ch1_pgs.rjust(lineWidth/2)
 puts ch2.ljust(lineWidth/2) + ch2_pgs.rjust(lineWidth/2)
 puts ch3.ljust(lineWidth/2) + ch3_pgs.rjust(lineWidth/2)
+puts
