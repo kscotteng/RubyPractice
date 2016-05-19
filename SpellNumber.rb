8fe1c54c1423878df93b24a87b0be2c2d9c25357
kscotteng <kscotteng@gmail.com>
2016-05-18 22:20:38 -0400

    initial commit

---
+++ SpellNumber.rb
@@ -0,0 +1,15 @@
+# this program will take a number from 0 - 100 and spell it out
+
+def spellNumber (number)
+  if number < 0
+    puts 'Please enter a number greater than 0'
+  elsif number > 100
+    puts 'Please enter a number less than 100'
+  elsif
+    puts number
+  end
+end
+
+spellNumber(-5)
+spellNumber(110)
+spellNumber(54)
