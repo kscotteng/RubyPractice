5c7e5be0f59e350e7c6b4694daf645bdef988fa1
kscotteng <kscotteng@gmail.com>
2016-05-18 22:54:35 -0400

    work on initial calculations that will be used to convert numbers to strings

--- SpellNumber.rb
+++ SpellNumber.rb
@@ -10,6 +10,19 @@ def spellNumber (number)
   end
 end
 
-spellNumber(-5)
-spellNumber(110)
-spellNumber(54)
+# spellNumber(-5)
+# spellNumber(110)
+# spellNumber(54)
+
+# number spelled out and returned as a string
+numString = ''
+
+# 'remaining' will be the remainder of the number to be converted to a string
+remaining = number
+
+# 'spell' is the portion of the number currently being converted to a string
+# here, we are determining how many hundreds remain to be converted to a string
+spell = remaining / 100
+
+# here, we are subracting the hundreds previously mentioned
+remaining = remaining - spell * 100
