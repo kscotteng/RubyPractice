9456b764e26a1d9299a475436966289a87504924
kscotteng <kscotteng@gmail.com>
2016-05-21 22:53:22 -0400

    refactor initial spellNumber method

--- SpellNumber2.rb
+++ SpellNumber2.rb
@@ -1,13 +1,9 @@
 # this program will take a number from 0 - 1000 and spell it out
 
 def spellNumber (number)
-  # number has to be at least zero
-  if number < 0
-    puts 'Please enter a number greater than 0'
-  # can not be greater than 1000
-  elsif number > 1000
-    puts 'Please enter a number less than 1000'
-  # elsif
-  #   puts number
+  # number has to be at least zero and can not be greater than 1000
+  if number < 0 || number > 1000
+    return 'Please enter a number between 0 and 1000'
   end
+
 end
