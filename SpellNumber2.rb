02d7a3fff78a02f346a53914e7c970f7acc9a8f9
kscotteng <kscotteng@gmail.com>
2016-05-26 20:35:17 -0400

    increased range to 10000, added arrays ones, tens and teens

--- SpellNumber2.rb
+++ SpellNumber2.rb
@@ -1,13 +1,18 @@
-# this program will take a number from 0 - 1000 and spell it out
+# this program will take an integer from 0 - 10000 and spell it out
 
 def spellNumber (number)
-  # number has to be at least zero and can not be greater than 1000
-  if number < 0 || number > 1000
-    return 'Please enter a number between 0 and 1000'
+
+  # number has to be at least zero and can not be greater than 10000
+  if number < 0 || number >= 10000
+    return 'Please enter a number between 0 and 10000'
   end
 
-  # number spelled out and returned as a string
+  # creates an empty string
   numString = ''
+
+  ones = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+  tens = ['ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+  teens = ['eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
 
   # 'remaining' is the remainder of the number to be converted to a string
   remaining = number
