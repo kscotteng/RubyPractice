6b6ce1cdc19fa27dacd05981a84c907c7fecfcdb
kscotteng <kscotteng@gmail.com>
2016-05-29 23:38:30 -0400

    convert thousands place to string

--- WeddingInvite.rb
+++ WeddingInvite.rb
@@ -5,7 +5,7 @@ year = gets.chomp.to_i
 
 def weddingYear(year)
 
-  if year < 2016 || number > 9999
+  if year < 2016 || year > 9999
     return 'Please enter a year 2016 or later'
   end
 
@@ -14,5 +14,17 @@ def weddingYear(year)
   ones = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
   tens = ['tens', 'twenty', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
   teens = ['eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+
+  remaining = year
+  spell = remaining / 1000
+  remaining = remaining - spell * 1000
+
+  if spell > 0
+    thousands = weddingYear(spell)
+    numString = numString + thousands + ' thousand'
+    if remaining > 0
+      numString = numString + ' '
+    end
+  end
 
   
