c0b34a204ae838633a412564a2981b976ba2e32e
kscotteng <kscotteng@gmail.com>
2016-05-29 23:25:42 -0400

    begin weddingYear method, define year range, define arrays for year string values

--- WeddingInvite.rb
+++ WeddingInvite.rb
@@ -2,3 +2,17 @@
 
 puts "What year will you be getting married?"
 year = gets.chomp.to_i
+
+def weddingYear(year)
+
+  if year < 2016 || number > 9999
+    return 'Please enter a year 2016 or later'
+  end
+
+  numString = ''
+
+  ones = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+  tens = ['tens', 'twenty', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+  teens = ['eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+
+  
