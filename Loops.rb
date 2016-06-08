1ac6df02456f6a6881c2d47a07aa71f09d7c7dc3
kscotteng <kscotteng@gmail.com>
2016-06-08 13:09:59 -0400

    initial commit for ruby loop examples

--- Loops.rb
+++ Loops.rb
@@ -1,8 +1,22 @@
-command = ''
+# Ruby loops:
+# a loop will run forever until a condition is met
+#     - meaning the condition to exit returns a value of true
+# an infinite loop is created when the condition never returns true
 
-while command != 'bye'
-  command = gets.chomp
-  puts command
-end
 
-puts 'Come again soon!'
+
+# the following code will guess a number bewteen zero and five
+# generating a random number (random.new) and give it an argument, ex.(5), for the max value of the random number
+
+random_number = Random.new.rand(5)
+
+
+
+# command = ''
+#
+# while command != 'bye'
+#   command = gets.chomp
+#   puts command
+# end
+#
+# puts 'Come again soon!'
